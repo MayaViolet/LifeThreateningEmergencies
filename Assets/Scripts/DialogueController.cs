a4c182620e40c4738d9e2b2028ce2296efa28b8a
Maya Kerr <maya@violetmay.net>
2015-01-24 00:14:24 +1100

    Added placeholder dialogue display

---
+++ Assets/Scripts/DialogueController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using DaikonForge;
+using DaikonForge.Tween;
+
+public class DialogueController : MonoBehaviour {
+
+	public Text dialogueText;
+	public Image portraitImage;
+	public RectTransform dialogueBase;
+
+	Vector3 showPosition;
+	Vector3 hidePosition;
+
+	public bool visible
+	{
+		set
+		{
+			if (value)
+			{
+				dialogueBase.TweenMoveTo(showPosition)
+					.SetDuration(0.5f)
+					.SetEasing(TweenEasingFunctions.EaseOutBack)
+					.Play();
+			}
+			else
+			{
+				dialogueBase.TweenMoveTo(hidePosition)
+					.SetDuration(0.5f)
+					.SetEasing(TweenEasingFunctions.EaseInBack)
+					.Play();
+			}
+		}
+	}
+
+	void Start()
+	{
+		showPosition = dialogueBase.position;
+		hidePosition = showPosition + dialogueBase.rect.height * 2 * Vector3.down;
+		dialogueBase.position = hidePosition;
+	}
+}
