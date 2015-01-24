fb7004d28443465d214f71b337682a5224c69402
Maya Kerr <maya@violetmay.net>
2015-01-24 17:10:11 +1100

    Added simple music player script

---
+++ Assets/Shared/MusicPlayer.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class MusicPlayer : MonoBehaviour {
+
+	static MusicPlayer _instance;
+
+	public float fadeTime;
+	float fadeTimer;
+	float startVolume;
+	AudioClip nextTrack;
+
+	void Awake() {
+		if (_instance)
+		{
+			Destroy(gameObject);
+			return;
+		}
+		_instance = this;
+		DontDestroyOnLoad(gameObject);
+		startVolume = audio.volume;
+	}
+
+	void Update () {
+		if (nextTrack != null)
+		{
+			fadeTimer -= Time.deltaTime;
+			float volumeMultiplier = fadeTimer / fadeTime;
+			audio.volume = volumeMultiplier * startVolume;
+			if (fadeTimer <= 0)
+			{
+				audio.clip = nextTrack;
+				audio.Play();
+				nextTrack = null;
+				audio.volume = startVolume;
+			}
+		}
+	}
+
+	public static void PlayTrack(AudioClip track)
+	{
+		_instance.nextTrack = track;
+		_instance.fadeTimer = _instance.fadeTime;
+	}
+}
---
+++ Assets/Shared/SceneMusic.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneMusic : MonoBehaviour {
+
+	public AudioClip track;
+
+	// Use this for initialization
+	void Start () {
+		MusicPlayer.PlayTrack(track);
+	}
+}
