fa8ad6c539333b94e6a80626c233f1d8c6f2f331
Maya Kerr <maya@violetmay.net>
2015-01-24 15:52:49 +1100

    Added exposure control effect

---
+++ Assets/Scripts/ExposureEffect.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+[ExecuteInEditMode(), RequireComponent(typeof(Camera))]
+class ExposureEffect : MonoBehaviour {
+
+	[Range(0, 2)]
+	public float exposure = 1;
+	float lastExposure = 1;
+
+	Material _material;
+	Material material
+	{
+		get
+		{
+			if (_material == null)
+			{
+				Shader shader = Shader.Find("Unlit/Texture + Colour");
+				_material = new Material(shader);
+				_material.color = new Color(exposure/2, exposure/2, exposure/2, 1);
+			}
+			return _material;
+		}
+	}
+
+	void OnRenderImage (RenderTexture source, RenderTexture destination)	
+	{
+		if (exposure != lastExposure)
+		{
+			material.color = new Color(exposure/2, exposure/2, exposure/2, 1);
+		}
+		lastExposure = exposure;
+		Graphics.Blit(source, destination, material);
+	}
+		
+}
