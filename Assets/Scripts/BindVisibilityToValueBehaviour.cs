b47b8664ff9ff77a632defb16f84364097c95ee2
Yuki Izumi <yuki@kivikakk.ee>
2015-01-25 00:19:40 +1100

    Use a fade on the onion. Need to refine.

---
+++ Assets/Scripts/BindVisibilityToValueBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BindVisibilityToValueBehaviour : MonoBehaviour {
+	public enum VisibleWhen	{
+		True,
+		False
+	};
+	
+	public VisibleWhen VisibleWhenFlagIs = VisibleWhen.False;
+	public string Flag;
+	
+	void Start () {
+		ValueStore.OnValueChanged (Flag, value => {
+			gameObject.GetComponent<SpriteRenderer>().enabled = (VisibleWhenFlagIs == VisibleWhen.False) ? !value : value;
+		});
+	}
+	
+	void Update () {
+	
+	}
+}
