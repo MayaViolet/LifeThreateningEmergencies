fe7d80d4eb1df837acc9c4180fc1ecfae2df492e
Maya Kerr <maya@violetmay.net>
2015-01-24 16:22:00 +1100

    Added scene transition with fade in/out

---
+++ Assets/Scripts/GameCamera.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using DaikonForge;
+using DaikonForge.Tween;
+
+public class GameCamera : MonoBehaviour {
+
+	ExposureEffect exposure;
+
+	void Start () {
+		TransitionHandler.Instance.OnTransition += OnSceneTransition;
+		//fade in
+		exposure = GetComponent<ExposureEffect>();
+		exposure.TweenProperty<float>("exposure")
+			.SetStartValue(0)
+			.SetEndValue(1)
+			.SetDuration(0.5f)
+			.SetEasing(TweenEasingFunctions.EaseOutQuad)
+			.Play();
+	}
+
+	void FadeOut(float duration)
+	{
+		exposure.TweenProperty<float>("exposure")
+			.SetStartValue(1)
+			.SetEndValue(0)
+			.SetDuration(duration)
+			.SetEasing(TweenEasingFunctions.EaseInQuad)
+			.Play();
+	}
+
+	void OnSceneTransition(float duration)
+	{
+		FadeOut(duration);
+	}
+
+}
---
+++ Assets/Scripts/TransitionHandler.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TransitionHandler : MonoBehaviour {
+
+	static TransitionHandler _instance;
+	public static TransitionHandler Instance
+	{
+		get
+		{
+			if (_instance == null)
+			{
+				new GameObject("TransitionHandler", typeof(TransitionHandler));
+			}
+			return _instance;
+		}
+	}
+
+	public delegate void TransitionListener(float duration);
+	public event TransitionListener OnTransition;
+
+	const float TRANSITION_TIME = 0.5f;
+
+	void Awake()
+	{
+		if (_instance != null)
+		{
+			Destroy(gameObject);
+			return;
+		}
+		_instance = this;
+	}
+
+	public void TransitionTo(string sceneName)
+	{
+		StartCoroutine(TransitionRoutine(sceneName));
+	}
+
+	IEnumerator TransitionRoutine(string nextScene)
+	{
+		if (OnTransition != null)
+		{
+			OnTransition(TRANSITION_TIME);
+		}
+		yield return new WaitForSeconds(TRANSITION_TIME);
+		Application.LoadLevel(nextScene);
+	}
+}
