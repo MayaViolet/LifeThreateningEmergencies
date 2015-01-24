87ce56b94967f62e194241d3e0b86356735586de
Maya Kerr <maya@violetmay.net>
2015-01-24 23:07:46 +1100

    Added npc controller for sprite ordering and responding to player and dialogue

---
+++ Assets/Scripts/NPCController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using BitterEnd;
+
+public class NPCController : MonoBehaviour {
+
+	public string characterName;
+
+	SpriteRenderer sprite;
+	SpriteRenderer child;
+
+	Transform player;
+
+	void Start () {
+		sprite  = renderer as SpriteRenderer;
+		child = transform.GetChild(0).renderer as SpriteRenderer;
+		GameObject playerGO = GameObject.FindGameObjectWithTag("PlayerController");
+		player = playerGO.transform;
+
+		GameObject dialogueGO = GameObject.FindGameObjectWithTag("DialogueController");
+		DialogueController dialogue = dialogueGO.GetComponent<DialogueController>();
+		dialogue.OnDisplayedLine += OnDialogue;
+	}
+
+	void LateUpdate () {
+		child.sortingOrder = sprite.sortingOrder+1;
+	}
+
+	void OnDialogue(DialogueLine nextLine)
+	{
+		if (nextLine.Character.Name == characterName)
+		{
+			animation.Play();
+		}
+
+		Vector3 scale = transform.localScale;
+		if (player.position.x < transform.position.x)
+		{
+			scale.x = Mathf.Abs(scale.x);
+		}
+		else
+		{
+			scale.x = -Mathf.Abs(scale.x);
+		}
+		transform.localScale = scale;
+	}
+}
