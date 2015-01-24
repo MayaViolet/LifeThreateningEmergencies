ef6ce18ff04ce9a1c495152b69fb7858d8af0029
Yuki Izumi <yuki@kivikakk.ee>
2015-01-24 17:21:48 +1100

    Some code formatting.

--- Assets/MusicLoopBehaviour.cs
+++ Assets/MusicLoopBehaviour.cs
@@ -1,30 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MusicLoopBehaviour : MonoBehaviour {
-	public AudioSource AudioSource;
-	public AudioClip[] Clips;
-	private int _clipCount;
-	private int _clipIndex;
-	private float _wait;
+public class MusicLoopBehaviour : MonoBehaviour
+{
+		public AudioSource AudioSource;
+		public AudioClip[] Clips;
+		private int _clipCount;
+		private int _clipIndex;
+		private float _wait;
 
-	void Start () {
-		_clipCount = Clips.Length;
-		PlayClip (0);
-	}
-
-	private void PlayClip(int index) {
-		_clipIndex = index;
-		AudioSource.clip = Clips [index];
-		_wait = Clips [index].length;
-		AudioSource.Play ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		_wait -= Time.deltaTime;
-		if (_wait < 0) {
-			PlayClip ((_clipIndex + 1) % _clipCount);
+		void Start ()
+		{
+				_clipCount = Clips.Length;
+				PlayClip (0);
 		}
-	}
+
+		private void PlayClip (int index)
+		{
+				_clipIndex = index;
+				AudioSource.clip = Clips [index];
+				_wait = Clips [index].length;
+				AudioSource.Play ();
+		}
+	
+		void Update ()
+		{
+				_wait -= Time.deltaTime;
+				if (_wait < 0) {
+						PlayClip ((_clipIndex + 1) % _clipCount);
+				}
+		}
 }
--- Assets/Scripts/AbstractInteraction.cs
+++ Assets/Scripts/AbstractInteraction.cs
@@ -4,35 +4,36 @@ using System;
 using System.Collections;
 using BitterEnd;
 
-abstract public class AbstractInteraction : MonoBehaviour, IPointerClickHandler {
-	protected DialogueController _dialogueController;
-	protected PlayerController _playerController;
+abstract public class AbstractInteraction : MonoBehaviour, IPointerClickHandler
+{
+		protected DialogueController _dialogueController;
+		protected PlayerController _playerController;
 	
-	void Start()
-	{
-		var dialogueControllerGO = GameObject.FindGameObjectWithTag("DialogueController");
-		_dialogueController = dialogueControllerGO.GetComponent<DialogueController>();
+		void Start ()
+		{
+				var dialogueControllerGO = GameObject.FindGameObjectWithTag ("DialogueController");
+				_dialogueController = dialogueControllerGO.GetComponent<DialogueController> ();
 		
-		var playerControllerGO = GameObject.FindGameObjectWithTag("PlayerController");
-		_playerController = playerControllerGO.GetComponent<PlayerController>();
-	}
-
-	void IPointerClickHandler.OnPointerClick (PointerEventData eventData)
-	{
-		var from = _playerController.transform.position;
-		var to = eventData.worldPosition;
-		to = new Vector3 (to.x, from.y, to.z);
-		
-		var diff = to - from;
-		var maximumDistance = GetMaximumDistance ();
-		if (diff.magnitude > maximumDistance) {
-			_playerController.MoveTo (from + diff.normalized * (diff.magnitude - maximumDistance), PerformInteraction);
-		} else {
-			PerformInteraction ();
+				var playerControllerGO = GameObject.FindGameObjectWithTag ("PlayerController");
+				_playerController = playerControllerGO.GetComponent<PlayerController> ();
 		}
-	}
-	
-	protected abstract void PerformInteraction ();
 
-	protected abstract float GetMaximumDistance ();
+		void IPointerClickHandler.OnPointerClick (PointerEventData eventData)
+		{
+				var from = _playerController.transform.position;
+				var to = eventData.worldPosition;
+				to = new Vector3 (to.x, from.y, to.z);
+		
+				var diff = to - from;
+				var maximumDistance = GetMaximumDistance ();
+				if (diff.magnitude > maximumDistance) {
+						_playerController.MoveTo (from + diff.normalized * (diff.magnitude - maximumDistance), PerformInteraction);
+				} else {
+						PerformInteraction ();
+				}
+		}
+	
+		protected abstract void PerformInteraction ();
+
+		protected abstract float GetMaximumDistance ();
 }
