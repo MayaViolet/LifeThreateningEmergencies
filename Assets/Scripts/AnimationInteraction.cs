c7d678b11a35177d2fca5e51685e680f7f224f60
Maya Kerr <maya@violetmay.net>
2015-01-24 13:31:30 +1100

    Added generic animation interaction handler and hooked up example in the lift

---
+++ Assets/Scripts/AnimationInteraction.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class AnimationInteraction : MonoBehaviour, IPointerClickHandler {
+
+	void IPointerClickHandler.OnPointerClick (PointerEventData eventData)
+	{
+		animation.Play();
+	}
+
+}
