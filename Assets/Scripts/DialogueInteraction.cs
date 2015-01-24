b36c1bffbf88883c70b722274c574247f07e406f
Maya Kerr <maya@violetmay.net>
2015-01-24 11:14:11 +1100

    Added dialogue interaction stub, set up scene for better layers and able to find dialogue controller by tag

---
+++ Assets/Scripts/DialogueInteraction.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+using BitterEnd;
+
+public class DialogueInteraction : MonoBehaviour, IPointerClickHandler {
+
+	public Dialogue dialogue;
+	DialogueController controller;
+
+	void Start()
+	{
+		GameObject controllerGO = GameObject.FindGameObjectWithTag("DialogueController");
+		controller = controllerGO.GetComponent<DialogueController>();
+	}
+
+	void IPointerClickHandler.OnPointerClick (PointerEventData eventData)
+	{
+
+	}
+
+}
