de789078ac512ff12e3d544e9feab606f78e7950
Maya Kerr <maya@violetmay.net>
2015-01-24 19:49:55 +1100

    Added tipping bucket to farm, made mess hall scene

--- Assets/Scripts/AnimationInteraction.cs
+++ Assets/Scripts/AnimationInteraction.cs
@@ -4,9 +4,15 @@ using System.Collections;
 
 public class AnimationInteraction : MonoBehaviour, IPointerClickHandler {
 
+	public bool singleUse;
+
 	void IPointerClickHandler.OnPointerClick (PointerEventData eventData)
 	{
 		animation.Play();
+		if (singleUse)
+		{
+			enabled = false;
+		}
 	}
 
 }
