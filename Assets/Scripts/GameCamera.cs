5317c57597f861925f9d099015f7aa55549182b0
Maya Kerr <maya@violetmay.net>
2015-01-24 18:31:12 +1100

    Added farm scene, added camera follow with background constraint and deadzone

--- Assets/Scripts/GameCamera.cs
+++ Assets/Scripts/GameCamera.cs
@@ -5,6 +5,10 @@ using DaikonForge.Tween;
 
 public class GameCamera : MonoBehaviour {
 
+	public Transform target;
+	public Vector3 offset;
+	public SpriteRenderer background;
+	public float deadZone = 0.5f;
 	ExposureEffect exposure;
 
 	void Start () {
@@ -32,6 +36,38 @@ public class GameCamera : MonoBehaviour {
 	void OnSceneTransition(float duration)
 	{
 		FadeOut(duration);
+	}
+
+	void LateUpdate()
+	{
+		Vector2 targetDirection = target.position + offset - transform.position;
+		if (targetDirection.magnitude > deadZone)
+		{
+			Vector3 position = transform.position;
+			targetDirection.Normalize();
+			Vector3 newPosition = (target.position + offset) - (Vector3)(targetDirection * deadZone);
+			newPosition.z = position.z;
+			transform.position = newPosition;
+		}
+		if (background != null)
+		{
+			ConstrainToBackground();
+		}
+	}
+
+	void ConstrainToBackground()
+	{
+		float camWidth = Screen.width / (float)Screen.height;
+		camWidth *= camera.orthographicSize;
+
+		Bounds spriteRect = background.sprite.bounds;
+		float maxX = spriteRect.size.x/2 - camWidth;
+		float maxY = spriteRect.size.y/2 - camera.orthographicSize;
+
+		Vector3 position = transform.position;
+		position.x = Mathf.Clamp(position.x, -maxX, maxX);
+		position.y = Mathf.Clamp(position.y, -maxY, maxY);
+		transform.position = position;
 	}
 
 }
