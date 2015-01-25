be27145cb9a218dc170448a69b195ccf0ec5135e
Yuki Izumi <yuki@kivikakk.ee>
2015-01-25 14:51:31 +1100

    Generalise MainMenuAfterAnimation.

--- Assets/Scripts/MainMenuAfterAnimation.cs
+++ Assets/Scripts/MainMenuAfterAnimation.cs
@@ -3,10 +3,12 @@ using System.Collections;
 
 public class MainMenuAfterAnimation : MonoBehaviour {
 
+	public string DestinationScene = "Title";
+
 	// Update is called once per frame
 	void Update () {
 		if (!animation.isPlaying) {
-			TransitionHandler.Instance.TransitionTo("Title");
+			TransitionHandler.Instance.TransitionTo(DestinationScene);
 		}
 	}
 }
